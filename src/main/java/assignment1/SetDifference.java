4e4db9991bb54aed836e0a5c39f1806110791183
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2026-03-24 11:42:03 +0200

    Project structure

---
+++ src/main/java/assignment1/SetDifference.java
@@ -0,0 +1,30 @@
+package assignment1;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
+public class SetDifference {
+
+    public static class SetDifferenceMapper extends Mapper<LongWritable, Text, Text, Text> {
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // Write mapping logic here
+        }
+    }
+
+    public static class SetDifferenceReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            // Write reduce logic here
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Job configuration logic
+    }
+}
